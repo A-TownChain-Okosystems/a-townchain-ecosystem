e0caeeb3e5a097636d8d3820d111f3a905c10d71
ShivaCoreDev <michael.worob@gmail.com>
2026-09-18 17:47:15 +0200

    update E2E for signed consensus finality

--- components/atc-sdk/tests/e2e.rs
+++ components/atc-sdk/tests/e2e.rs
@@ -9,7 +9,7 @@ fn sdk_node_mempool_consensus_vm_state_storage_indexer(){
  let key=SigningKey::from_bytes(&[7u8;32]);let tx=TransactionBuilder::transfer(chain_id,"alice","bob",100,1,1000,0,2).sign(&key);
  let txid=tx.id;node.submit(tx,2).unwrap();assert_eq!(node.pool.get_pending_batch(10).len(),1);
  let block=node.produce(3,10).unwrap();assert_eq!(block.transactions[0].id,txid);assert_eq!(node.state.balance("bob"),100);assert!(indexer.blocks().is_empty());
- assert!(node.finalize(&block,1).unwrap());assert!(indexer.contains(block.id));assert_eq!(node.storage.block(1).unwrap().id,block.id);assert_eq!(node.storage.state_root(1),Some(node.state.root()));
+ let vote_key=SigningKey::from_bytes(&[6u8;32]);let voter="validator-1".to_string();let mut vote=atc_blockchain::consensus::Vote{block:block.id,voter:voter.clone(),approve:true,signature:[0;64],public_key:vote_key.verifying_key().to_bytes()};let mut vb=Vec::new();vb.extend_from_slice(b"ATC-VOTE-V1");vb.extend_from_slice(&chain_id.to_be_bytes());vb.extend_from_slice(&vote.block);vb.push(1);vb.extend_from_slice(&(voter.len()as u32).to_be_bytes());vb.extend_from_slice(voter.as_bytes());vote.signature=vote_key.sign(&vb).to_bytes();node.submit_vote(vote).unwrap();assert!(node.finalize(&block,1).unwrap());assert!(indexer.contains(block.id));assert_eq!(node.storage.block(1).unwrap().id,block.id);assert_eq!(node.storage.state_root(1),Some(node.state.root()));
 }
 
 #[test]
