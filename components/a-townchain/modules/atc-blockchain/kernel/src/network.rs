e40818cac62139d11cd77c36e67e42d411fae556
ShivaCoreDev <michael.worob@gmail.com>
2026-09-19 00:51:44 +0200

    fix(atc-blockchain): correct peer transport trait syntax

--- components/a-townchain/modules/atc-blockchain/kernel/src/network.rs
+++ components/a-townchain/modules/atc-blockchain/kernel/src/network.rs
@@ -1,2 +1,20 @@
 //! P2P protocol boundary; transport is intentionally outside consensus state.
-#[derive(Clone,Debug,PartialEq,Eq)]pub enum NetworkMessage{Transaction([u8;32]),Block([u8;32]),Vote([u8;32])}pub trait PeerTransport:Send+Sync{fn broadcast(&self,message:NetworkMessage)->Result<(),String>}pub struct NullTransport;impl PeerTransport for NullTransport{fn broadcast(&self,_:NetworkMessage)->Result<(),String>{Ok(())}}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum NetworkMessage {
+    Transaction([u8; 32]),
+    Block([u8; 32]),
+    Vote([u8; 32]),
+}
+
+pub trait PeerTransport: Send + Sync {
+    fn broadcast(&self, message: NetworkMessage) -> Result<(), String>;
+}
+
+pub struct NullTransport;
+
+impl PeerTransport for NullTransport {
+    fn broadcast(&self, _message: NetworkMessage) -> Result<(), String> {
+        Ok(())
+    }
+}
