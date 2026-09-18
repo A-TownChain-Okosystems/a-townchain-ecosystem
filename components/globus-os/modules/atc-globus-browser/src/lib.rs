e6662488337623accbc5071d33bce320bc3658ba
ShivaCoreDev <michael.worob@gmail.com>
2026-09-18 15:24:27 +0200

    feat(browser): expose renderer capability boundary

--- components/globus-os/modules/atc-globus-browser/src/lib.rs
+++ components/globus-os/modules/atc-globus-browser/src/lib.rs
@@ -4,6 +4,7 @@ mod content;
 mod error;
 mod history;
 mod policy;
+mod renderer;
 #[allow(missing_docs)]
 mod tabs;
 
@@ -12,4 +13,5 @@ pub use content::{ContentKind, RenderInput};
 pub use error::BrowserError;
 pub use history::{History, HistoryEntry};
 pub use policy::BrowserPolicy;
+pub use renderer::{PassthroughRenderer, RenderedDocument, Renderer};
 pub use tabs::{Tab, TabId, TabManager};
