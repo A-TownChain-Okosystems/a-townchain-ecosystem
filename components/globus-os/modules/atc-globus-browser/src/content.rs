5bbcd1fa6921146df494c687757185600cf6a384
ShivaCoreDev <michael.worob@gmail.com>
2026-09-19 00:46:03 +0200

    ci: apply rustfmt to Globus browser module

--- components/globus-os/modules/atc-globus-browser/src/content.rs
+++ components/globus-os/modules/atc-globus-browser/src/content.rs
@@ -66,10 +66,10 @@ mod tests {
 
     #[test]
     fn classifies_common_web_content() {
-        assert_eq!(ContentKind::from_content_type(Some("text/html; charset=utf-8")), ContentKind::Html);
-        assert_eq!(ContentKind::from_content_type(Some("text/css")), ContentKind::Css);
-        assert_eq!(ContentKind::from_content_type(Some("application/json")), ContentKind::Json);
-        assert_eq!(ContentKind::from_content_type(Some("image/png")), ContentKind::Image);
+        assert_eq!(\n            ContentKind::from_content_type(Some("text/html; charset=utf-8")),\n            ContentKind::Html\n        );
+        assert_eq!(\n            ContentKind::from_content_type(Some("text/css")),\n            ContentKind::Css\n        );
+        assert_eq!(\n            ContentKind::from_content_type(Some("application/json")),\n            ContentKind::Json\n        );
+        assert_eq!(\n            ContentKind::from_content_type(Some("image/png")),\n            ContentKind::Image\n        );
     }
 
     #[test]
