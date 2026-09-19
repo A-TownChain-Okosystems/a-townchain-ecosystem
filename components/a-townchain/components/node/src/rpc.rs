a93933de0341421a35c133ef5bf7887057e8605b
ShivaCoreDev <michael.worob@gmail.com>
2026-09-19 08:03:03 +0200

    fix(node): simplify incoming RPC stream handling

--- components/a-townchain/components/node/src/rpc.rs
+++ components/a-townchain/components/node/src/rpc.rs
@@ -3,7 +3,7 @@
 
 use crate::bootstrap::Genesis;
 use crate::peers::PeerTable;
-use std::io::{BufRead, BufReader, Write};
+use std::io::{BufRead, BufReader};
 use std::net::{TcpListener, TcpStream};
 
 pub struct DevnetRpc {
@@ -34,10 +34,8 @@ impl DevnetRpc {
 
 pub fn serve(addr: &str, state: DevnetRpc) -> std::io::Result<()> {
     let listener = TcpListener::bind(addr)?;
-    for stream in listener.incoming() {
-        if let Ok(stream) = stream {
-            handle(stream, &state)?;
-        }
+    for stream in listener.incoming().flatten() {
+        handle(stream, &state)?;
     }
     Ok(())
 }
