23ca298a4ef20b9441280db0c6084359744d91aa
ShivaCoreDev <michael.worob@gmail.com>
2026-09-19 08:01:03 +0200

    fix(security): remove unused scanner import

--- components/atc-shivacore/modules/atc-security/src/scanner.rs
+++ components/atc-shivacore/modules/atc-security/src/scanner.rs
@@ -1,7 +1,5 @@
 // Copyright (c) 2026 Michael Wroblewski / ShivaCore / A-TownChain-Okosystems. All Rights Reserved.
 // Vulnerability scanner
-use std::collections::HashMap;
-
 pub struct VulnerabilityScanner {
     findings: Vec<ScanResult>,
 }
