c9fe6b14a52e57709383a7833ebd9d702d03ab5e
ShivaCoreDev <michael.worob@gmail.com>
2026-09-20 00:59:05 +0200

    style: apply rustfmt to L1 and wallet workspace

--- components/atc-marketplace/modules/atc-dex/src/fees.rs
+++ components/atc-marketplace/modules/atc-dex/src/fees.rs
@@ -6,14 +6,18 @@ pub struct FeeCalculator {
 impl FeeCalculator {
     pub const DEFAULT_RATE_BPS: u64 = 30;
 
-    pub fn new(rate_bps: u64) -> Self { Self { rate_bps } }
+    pub fn new(rate_bps: u64) -> Self {
+        Self { rate_bps }
+    }
     pub fn calculate_fee(&self, amount: u128) -> u128 {
         amount.saturating_mul(self.rate_bps as u128) / 10_000
     }
     pub fn calculate_net(&self, amount: u128) -> u128 {
         amount.saturating_sub(self.calculate_fee(amount))
     }
-    pub fn rate_bps(&self) -> u64 { self.rate_bps }
+    pub fn rate_bps(&self) -> u64 {
+        self.rate_bps
+    }
 }
 
 #[cfg(test)]
