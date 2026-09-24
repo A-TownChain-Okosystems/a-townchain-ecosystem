4e6a78315093b2a45f041cd8d900e378ede0200a
ShivaCoreDev <michael.worob@gmail.com>
2026-09-24 18:41:11 +0200

    feat(protocol): decode validator transaction type

--- components/a-townchain/modules/atc-blockchain/kernel/src/storage.rs
+++ components/a-townchain/modules/atc-blockchain/kernel/src/storage.rs
@@ -81,6 +81,7 @@ fn tx_decode(b: &[u8], p: &mut usize) -> Result<Transaction, String> {
             1 => TxType::Stake,
             2 => TxType::Unstake,
             3 => TxType::Contract,
+            4 => TxType::Validator,
             _ => return Err("invalid tx type".into()),
         }
     };
