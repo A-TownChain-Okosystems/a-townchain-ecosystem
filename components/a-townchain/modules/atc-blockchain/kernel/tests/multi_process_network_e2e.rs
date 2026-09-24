f8aca14c2a32ea3b36daa793dd7ae9b82ae0ac70
ShivaCoreDev <michael.worob@gmail.com>
2026-09-24 16:42:39 +0200

    fix(network-e2e): wait for second-block finality before restart

--- components/a-townchain/modules/atc-blockchain/kernel/tests/multi_process_network_e2e.rs
+++ components/a-townchain/modules/atc-blockchain/kernel/tests/multi_process_network_e2e.rs
@@ -118,7 +118,14 @@ fn run_initial_node_a() {
     let block2 = node.produce_reward_block(3).unwrap();
     assert_eq!(block2.height, 2);
     assert_eq!(node.chain.height(), 2);
-    loop_handle.thread().unpark();
+    for _ in 0..120 {
+        if node.consensus.finalized().map(|x| x.0) == Some(block2.height) {
+            break;
+        }
+        thread::sleep(Duration::from_millis(25));
+    }
+    assert_eq!(node.consensus.finalized().map(|x| x.0), Some(block2.height));
+    drop(loop_handle);
 }
 
 fn run_initial_node_b() {
