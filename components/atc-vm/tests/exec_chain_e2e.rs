b6a207665c7b4d916715446934b07186a62ce4d1
ShivaCoreDev <michael.worob@gmail.com>
2026-09-25 13:49:57 +0200

    style: apply rustfmt to vm sources

--- components/atc-vm/tests/exec_chain_e2e.rs
+++ components/atc-vm/tests/exec_chain_e2e.rs
@@ -51,7 +51,8 @@ fn div_executes_deterministically() {
 
 #[test]
 fn div_by_zero_fails_closed() {
-    let mut machine = Vm::new(vec![Op::Push(1), Op::Push(0), Op::Div, Op::Halt]).expect("ATVM-Verifier");
+    let mut machine =
+        Vm::new(vec![Op::Push(1), Op::Push(0), Op::Div, Op::Halt]).expect("ATVM-Verifier");
     let res = machine.run();
     assert_eq!(res, Err(atc_vm::vm::VmError::DivisionByZero));
 }
